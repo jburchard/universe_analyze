1586f9ee84d1075101afff958e8e0c3dce5f4bfd
jburchard <jburchard@gmail.com>
2022-05-09 08:17:32 -0500

    Improve uploader.

--- README.md
+++ README.md
@@ -8,4 +8,13 @@ Make sure `.venv` is activated.
 ``` source .venv/bin/activate ```
 
 Generate requirements file.
+``` pip freeze > requirements.txt ```
 
+## To Do:
+- Malformed or null zipcodes.
+- Drop the bad records.
+- Download all of the bad records.
+- Householding detection.
+- Change column header names.
+- Intelligent Mapping.
+- Duplicate column detection.
--- mailtrack.py
+++ mailtrack.py
@@ -1,7 +1,8 @@
-import os
 import streamlit as st
 import pandas as pd
-import petl as etl
+from utilities import form_values
+from io import StringIO
+
 
 st.set_page_config(page_title="Universe Validate", page_icon="📆", layout="wide")
 st.title("Universe Analyze")
@@ -10,25 +11,30 @@ st.selectbox('Universe Type', ['Mail'])
 
 st.header("Upload File")
 uploaded_file = st.file_uploader("Upload a csv")
-delimiter = st.selectbox("Delimiter", ['Comma','Tab'])
+csv_settings = st.columns([1,1,1])
+delimiter = csv_settings[0].selectbox("Delimiter", ['Comma','Tab','Pipe','Custom Tab'])
 if delimiter == 'Comma':
     delimiter = ','
 if delimiter == 'Tab':
-    delimiter == '\t'
+    delimiter = "\t"
 if delimiter == 'Pipe':
     delimiter = '|'
+encoding = csv_settings[1].selectbox("Encoding", ['utf_8', 'utf_16'])
+encoding_errors = csv_settings[2].selectbox("Encoding Errors", ['strict', 'ignore'])
+
+print (delimiter)
 
 if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file, sep=delimiter, encoding=encoding, encoding_errors=encoding_errors)
     with st.expander('View Sample Records') as exp:
-        df = pd.read_csv(uploaded_file, sep=delimiter, dtype=str)
-        uploaded_file.seek(0)
-        st.write(df[:100])
+            uploaded_file.seek(0)
+            st.write(df[:100])
 
     st.header("Configuration")
     st.caption("Map the columns and enter expected values.")
     map_cols = st.columns([1,1,2])
     state = map_cols[0].selectbox('State Column', df.columns.values.tolist())
-    valid_states = map_cols[1].text_input('Expected States', help="A list of states you expect to see in the file. Separate by multiple states comma. e.g. (IL, WI).")
+    valid_states = map_cols[1].multiselect('Expected States', form_values.states, help="You may select multiple states.")
     district = map_cols[0].selectbox('District Column', df.columns.values.tolist())
     valid_districts = map_cols[1].text_input('Expected Districts', help="A list of districts you expect to see in the file. Separate by multiple states comma. e.g. (01, 02).")
     
@@ -37,7 +43,8 @@ if uploaded_file is not None:
         return [i.strip() for i in input]
         
     def out_of_geo_count(df, col, valid_values):
-        valid_values = clean_text_input(valid_values)
+        if type(valid_values) == str:
+            valid_values = clean_text_input(valid_values)
         out_of_geo = df[col].count() - df[col][df[col].isin(valid_values)].count()
         total = out_of_geo + df[col].isna().sum()
         return total
@@ -47,5 +54,5 @@ if uploaded_file is not None:
     report_col[0].metric('Records', len(df))
     if len(valid_states) > 0: 
         report_col[1].metric('Out Of State Or State Null', out_of_geo_count(df, state, valid_states))
-    if len(valid_districts) > 0: report_col[2].metric('Out Of District Or District Null', out_of_geo_count(df, district, valid_districts))
-
+    if len(valid_districts) > 0: 
+        report_col[2].metric('Out Of District Or District Null', out_of_geo_count(df, district, valid_districts))
